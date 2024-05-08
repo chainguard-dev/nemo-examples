915ede5a1523f4a9390f7d2ec5f71ed712cf7dde
Patrick Smyth <patrick.smyth@chainguard.dev>
2024-05-08 15:59:02 -0400

    Updated TTS script
    Signed-off-by: Patrick Smyth <patrick.smyth@chainguard.dev>

--- quickstart.py
+++ quickstart.py
@@ -7,18 +7,24 @@ from nemo.collections import asr, nlp, tts
 import requests
 
 # Download an audio file that we will transcribe, translate, and convert to speech
-r = requests.get('https://nemo-public.s3.us-east-2.amazonaws.com/zh-samples/common_voice_zh-CN_21347786.mp3')
-with open('voice.mp3', 'wb') as f:
+r = requests.get(
+    "https://nemo-public.s3.us-east-2.amazonaws.com/zh-samples/common_voice_zh-CN_21347786.mp3"
+)
+with open("voice.mp3", "wb") as f:
     f.write(r.content)
 
 
 # Instantiate a Mandarin speech recognition model and transcribe an audio file.
-asr_model = asr.models.ASRModel.from_pretrained(model_name="stt_zh_citrinet_1024_gamma_0_25")
-mandarin_text = asr_model.transcribe(['voice.mp3'])
+asr_model = asr.models.ASRModel.from_pretrained(
+    model_name="stt_zh_citrinet_1024_gamma_0_25"
+)
+mandarin_text = asr_model.transcribe(["voice.mp3"])
 print(mandarin_text)
 
 # Instantiate Neural Machine Translation model and translate the text
-nmt_model = nlp.models.MTEncDecModel.from_pretrained(model_name="nmt_zh_en_transformer24x6")
+nmt_model = nlp.models.MTEncDecModel.from_pretrained(
+    model_name="nmt_zh_en_transformer24x6"
+)
 english_text = nmt_model.translate(mandarin_text)
 print(english_text)
 
--- tts.py
+++ tts.py
@@ -1,19 +1,28 @@
+from pathlib import Path
 import numpy as np
 from scipy.io.wavfile import write
 import torch
 from nemo.collections.tts.models.base import SpectrogramGenerator, Vocoder
 
+
+script_directory = Path(__file__).resolve().parents[0]
+
 if torch.cuda.is_available():
-  device = "cuda"
+    device = "cuda"
 else:
-  device = "cpu"
+    device = "cpu"
 
 # Load our spectrogram and vocoder models onto our CPU
-spectrogram_model = SpectrogramGenerator.from_pretrained("tts_en_tacotron2").eval().to(device)
+spectrogram_model = (
+    SpectrogramGenerator.from_pretrained("tts_en_tacotron2").eval().to(device)
+)
 vocoder = Vocoder.from_pretrained("tts_en_hifigan").eval().to(device)
 
 
-text = "Chainguard Images are built with security in mindfrom the ground up. They include fewer packages, a lighter footprint, included SBOMs, and active and ongoing CVE remediation."
+text = ' '.join([
+    "Chainguard Images are built with security in mindfrom the ground up.",
+    "They include fewer packages, a lighter footprint, included SBOMs, and active remediation"
+    ])
 
 # Normalize the text and convert it into individual phonemes/tokens.
 tokens = spectrogram_model.parse(text, normalize=True)
@@ -28,8 +37,6 @@ audio = vocoder.convert_spectrogram_to_audio(spec=spectrogram)
 spectrogram = spectrogram.cpu().detach().numpy()[0]
 audio = audio.cpu().detach().numpy()[0]
 
-rate = 15000
+RATE = 23000
 scaled = np.int16(audio / np.max(np.abs(audio)) * 32767)
-write('test.wav', rate, scaled)
-
-
+write(script_directory / Path("test.wav"), RATE, scaled)
